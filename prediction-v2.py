d81cd969c62360a1b56a183bd9a716446b74bcdc
Alessio Fantasia <a3214781@adelaide.edu.au>
2026-06-22 00:37:12 +0930

    Data cleaning and preprocessing

--- prediction-v2.py
+++ prediction-v2.py
@@ -1,9 +1,39 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("martj42/international-football-results-from-1872-to-2017")
-
-print("Path to dataset files:", path)
-
-
 # KGAT_b4bb4a3970b94af8d610d313c5b047aa
+# python3 prediction-v2.py
+
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("/Users/alessiofantasia/prediction-wc/data/results.csv", parse_dates=[0])
+
+# filter out all friendly matches
+data = data[data['tournament'] != 'Friendly']
+data = data.dropna(subset=['home_score', 'away_score'])
+data = data[data['date'] >= '2010-01-01']
+
+
+# np.where(condition, value_if_true, value_if_false)
+
+conditions = [
+    data['home_score'] > data['away_score'],  # Home win
+    data['away_score'] > data['home_score'],  # Away win
+    data['home_score'] == data['away_score']  # Draw
+]
+
+choices = [
+    data['home_team'],  # Result if condition 1 is True
+    data['away_team'],  # Result if condition 2 is True
+    'Draw'              # Result if condition 3 is True
+]
+
+# Create 'winner' column
+winner = np.select(conditions, choices)
+data['winner'] = winner
+
+# Dropping all draws from datased for binary regression (win or lose)
+data = data[data['winner'] != 'Draw']
+
+print(data['winner'].value_counts()) 
+
+
+# python3 prediction-v2.py
